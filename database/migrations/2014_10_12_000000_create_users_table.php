5d812d0394fb6d4d5bc3833bac6d5a39651d9d21
Andy Wright <atwright147@gmail.com>
2015-10-03 17:25:16 +0100

    oops missed the db migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -24,6 +24,7 @@ class CreateUsersTable extends Migration
             $table->string('avatar');
 
             $table->rememberToken();
+            $table->tinyInteger('admin');
             $table->timestamps();
         });
     }
